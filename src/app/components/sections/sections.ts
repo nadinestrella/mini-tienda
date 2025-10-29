4a9d8fefe7e3e0db32f8b4e5a32ea45d98653c47
nadinestrella <nadinestrella@gmail.com>
2025-10-29 14:03:06 +0100

    added new components

---
+++ src/app/components/sections/sections.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sections',
+  imports: [],
+  templateUrl: './sections.html',
+  styleUrl: './sections.css',
+})
+export class Sections {
+
+}
