b5aea3ffd6af3a2e39de3ada4034e96de3fc354a
nadinestrella <nadinestrella@gmail.com>
2025-11-24 11:19:46 +0100

    improved nav  menu

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  public headerOptions: { label: string; path: string; exact: boolean }[] = [
+    { label: 'Home', path: '/', exact: true },
+    { label: 'Products', path: '/products', exact: false },
+    { label: 'Contact', path: '/contact', exact: false },
+  ];
+}
